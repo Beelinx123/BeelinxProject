fb6c4ca0115bd3a093dd01685f181905f923b3e8
origin <demogit10@gmail.com>
2024-10-07 15:37:30 +0530

    Revert "login implementtion, signup implemention, getAllUser implemention"
    This reverts commit 2fb7617705019e29496d3620c60fc1db40a91a40.
    
    revert commit

--- src/main/java/com/beelinx/Services/UserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.beelinx.Services;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserDetailsService {
-
-    UserDetails loadUserByUsername(String username);
-}
